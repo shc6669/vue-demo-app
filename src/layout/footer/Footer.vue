a1d0c80eab063e6a39f4fc578384a8aa2925ae53
Chandrayana Putra <shc6669@gmail.com>
2022-03-24 00:16:27 +0800

    Fix template

--- src/layout/footer/Footer.vue
+++ src/layout/footer/Footer.vue
@@ -11,31 +11,15 @@
     >
       <!--begin::Copyright-->
       <div class="text-dark order-2 order-md-1">
-        <span class="text-muted fw-bold me-1">2021&copy;</span>
+        <span class="text-muted fw-bold me-1">{{year}} &copy;</span>
         <a
           href="https://keenthemes.com/"
           target="_blank"
           class="text-gray-800 text-hover-primary"
-          >Keenthemes</a
+          >Chandrayana Putra</a
         >
       </div>
       <!--end::Copyright-->
-
-      <!--begin::Menu-->
-      <ul class="menu menu-gray-600 menu-hover-primary fw-bold order-1">
-        <li class="menu-item">
-          <a href="#" target="_blank" class="menu-link px-2">About</a>
-        </li>
-
-        <li class="menu-item">
-          <a href="#" target="_blank" class="menu-link px-2">Support</a>
-        </li>
-
-        <li class="menu-item">
-          <a href="#" target="_blank" class="menu-link px-2">Purchase</a>
-        </li>
-      </ul>
-      <!--end::Menu-->
     </div>
     <!--end::Container-->
   </div>
@@ -49,8 +33,10 @@ import { footerWidthFluid } from "@/core/helpers/config";
 export default defineComponent({
   name: "KTFooter",
   setup() {
+    const year = new Date().getFullYear();
     return {
       footerWidthFluid,
+      year
     };
   },
 });
